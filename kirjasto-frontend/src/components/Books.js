7edeb22218aab63424f578af4395c347094dd0a4
Anssi Äijö <anssi.aijo@edu.karelia.fi>
2024-01-06 12:25:11 +0200

    8.19 ja 8.21 Genren kirjat

--- kirjasto-frontend/src/components/Books.js
+++ kirjasto-frontend/src/components/Books.js
@@ -1,17 +1,52 @@
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
+import { useState } from 'react'
 
 const Books = props => {
-  const result = useQuery(ALL_BOOKS)
+  //Etsityn genren muuttujatiedon säilyttämiseksi
+  const [genre, setGenre] = useState('')
+
+  //Parametrisoitu kyselymuoto
+  const { loading, error, data } = useQuery(ALL_BOOKS, {
+    variables: { genre },
+  })
 
   if (!props.show) {
     return null
   }
-  if (result.loading) {
+  if (loading) {
     return <div>loading...</div>
   }
+  if (error) {
+    return <div>Error {error.message}</div>
+  }
 
-  const books = result.data.allBooks
+  const books = data.allBooks
+
+  //Kerätään kirjojen genret listaan nappien generoimiseksi olemassaolevien genrejen pohjalta
+  //Tämän listan olisi hyvä olla muuttumaton
+  const genres = books.reduce((array, item) => {
+    const itGenres = [...item.genres]
+
+    itGenres.forEach(i => {
+      const check = array.includes(i)
+      if (!check) {
+        array.push(i)
+      }
+    })
+
+    return array
+  }, [])
+
+  const assignGenre = event => {
+    event.preventDefault()
+    setGenre(event.target.value)
+  }
+
+  const clearGenre = event => {
+    event.preventDefault()
+    setGenre('')
+  }
 
   return (
     <div>
@@ -25,7 +60,7 @@ const Books = props => {
             <th>published</th>
           </tr>
           {books.map(a => (
-            <tr key={a.title}>
+            <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
               <td>{a.published}</td>
@@ -33,6 +68,12 @@ const Books = props => {
           ))}
         </tbody>
       </table>
+      {genres.map(g => (
+        <button key={g} value={g} onClick={assignGenre}>
+          {g}
+        </button>
+      ))}
+      <button onClick={clearGenre}>show all</button>
     </div>
   )
 }
--- kirjasto-frontend/src/queries.js
+++ kirjasto-frontend/src/queries.js
@@ -10,13 +10,15 @@ export const ALL_AUTHORS = gql`
   }
 `
 export const ALL_BOOKS = gql`
-  query {
-    allBooks {
+  query getBooks($author: String, $genre: String) {
+    allBooks(author: $author, genre: $genre) {
       title
       author {
         name
       }
       published
+      genres
+      id
     }
   }
 `
